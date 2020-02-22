b2a367f0491fc19aa80d137c024003ae4221b88a
Hampus Skystedt <hampus@skystedt.se>
2020-02-22 20:34:29 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MicrosoftLocaleRedirect
-Chrome extension to redirect all localized URLs on microsoft.com to english (en-us)
+Chrome extension to redirect all localized URLs on microsoft.com to english (en-us).
+
+Uses [chrome.declarativeWebRequest beta API](https://developer.chrome.com/extensions/declarativeWebRequest). It is only available to Chrome users on the [beta channel](https://www.google.com/landing/chrome/beta/) and [dev channel](http://www.chromium.org/getting-involved/dev-channel).
+
+[Chrome Web Store](https://chrome.google.com/webstore/detail/emigmchappambenffnmfffpihlikcgko)
---
+++ src/main.js
@@ -0,0 +1,29 @@
+const microsoftLocalizedUrl = (locale) => `(https?://(?:[^\\.]+\\.)?microsoft.com)/${locale}/(.*)`;
+
+// https://developer.chrome.com/extensions/declarativeWebRequest#type-RedirectByRegEx
+chrome.declarativeWebRequest.onRequest.addRules([{
+    priority: 100,
+    conditions: [new chrome.declarativeWebRequest.RequestMatcher({
+        resourceType: ["main_frame"],
+        stages: ["onBeforeRequest"],
+        url: {
+            urlMatches: microsoftLocalizedUrl("..-..")
+        }
+    })],
+    actions: [new chrome.declarativeWebRequest.RedirectByRegEx({
+        from: microsoftLocalizedUrl("..-.."),
+        to: "$1/en-us/$2"
+    })]
+}, {
+    priority: 1000,
+    conditions: [new chrome.declarativeWebRequest.RequestMatcher({
+        resourceType: ["main_frame"],
+        stages: ["onBeforeRequest"],
+        url: {
+            urlMatches: microsoftLocalizedUrl("en-us")
+        }
+    })],
+    actions: [new chrome.declarativeWebRequest.IgnoreRules({
+        lowerPriorityThan: 1000
+    })]
+}]);
